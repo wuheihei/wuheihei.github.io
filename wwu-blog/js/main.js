23b59946fe336c2d8e357c5b18e24561387f10f5
wuheihei <39045004+wuheihei@users.noreply.github.com>
2025-12-10 16:13:13 +0800

    Delete wwu-blog directory

--- wwu-blog/js/main.js
+++
@@ -1,6 +0,0 @@
-// js/main.js
-function searchPosts() {
-    const query = document.getElementById('searchInput').value.toLowerCase();
-    alert(`搜索: ${query} - 这是简化的搜索功能`);
-    // 实际实现需要遍历文章内容
-}
